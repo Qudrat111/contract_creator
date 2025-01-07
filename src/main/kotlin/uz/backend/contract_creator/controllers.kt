67816f35447a03b7d3018e134fb28e6a56c6d473
Akromjon <sapabayevakromjon@gmail.com>
2025-01-07 17:01:39 +0500

    add upload file

--- src/main/kotlin/uz/backend/contract_creator/controllers.kt
+++ src/main/kotlin/uz/backend/contract_creator/controllers.kt
@@ -2,8 +2,9 @@ package uz.backend.contract_creator
 
 import org.springframework.context.support.ResourceBundleMessageSource
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.multipart.MultipartFile
 
 @ControllerAdvice
 class ExceptionHandler(private val errorMessageSource: ResourceBundleMessageSource) {
@@ -12,4 +13,14 @@ class ExceptionHandler(private val errorMessageSource: ResourceBundleMessageSour
     fun handleAccountException(exception: BaseExceptionHandler): ResponseEntity<BaseMessage> {
         return ResponseEntity.badRequest().body(exception.getErrorMessage(errorMessageSource))
     }
+}
+
+@RestController
+@RequestMapping("/attach")
+class AttachController(
+    private val service: AttachService,
+) {
+
+    @PostMapping("upload")
+    fun fileUpload(@RequestParam("file") file: MultipartFile) = service.saveToSystem(file)
 }
--- src/main/kotlin/uz/backend/contract_creator/services.kt
+++ src/main/kotlin/uz/backend/contract_creator/services.kt
@@ -6,7 +6,12 @@ import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
 import sun.security.jgss.GSSUtil.login
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
 
 interface AuthService : UserDetailsService {
     fun logIn(signInDTO: LogInDTO): TokenDTO?
@@ -40,5 +45,18 @@ class AuthServiceImpl(
     override fun loadUserByUsername(username: String): UserDetails {
 
        return userRepository.findByUsername(username) ?: throw RuntimeException("User $username not found")
+    }
+}
+
+@Service
+class AttachService {
+    fun saveToSystem(file: MultipartFile): String? {
+        val folder = File("Files")
+        if (!folder.exists()) folder.mkdir()
+
+        val bytes = file.bytes
+        val path: Path = Paths.get("Files/" + file.originalFilename)
+        Files.write(path, bytes)
+        return file.originalFilename
     }
 }
